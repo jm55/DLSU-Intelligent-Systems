82ecc562a1a9f8daeb2c5b2ed4e400250a476b03
Carla Jeannine Cruzada <80302664+CJOC3@users.noreply.github.com>
2023-02-11 14:34:46 +0800

    Add files via upload

--- MazeBot/Code/test_files/define_maze.py
+++ MazeBot/Code/test_files/define_maze.py
@@ -1,3 +1,5 @@
+import numpy as np
+from array import *
 def define_maze():
 
     with open("maze.txt") as maze_file:
@@ -13,9 +15,26 @@ def define_maze():
     #Make maze 2d array
     maze = []
     maze.append(temp)
-    print(maze)
+    #print(maze)
+    
+    maze = np.array(maze)
+
+    #test
+    # print(maze[0,0])
+    # print(maze[0,1])
+    # print(maze[0,2])
+    # print(maze[0,3])
+    # print(maze[0,4])
+    # print(maze[0,5])
+    # print(maze[0,6])
+    # print(maze[0,7])
+    # print(maze[0,8])
+    # print(maze[0,9])
+
+
+   
+
 
 if __name__ == "__main__":
     temp = []
     define_maze()
-
