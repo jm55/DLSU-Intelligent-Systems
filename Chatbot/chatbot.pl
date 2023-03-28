f9e68acc27ecfd2c0487f0dcf67cf4152ee5c33e
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-03-28 15:05:07 +0800

    Added treatment printouts

--- Chatbot/chatbot.pl
+++ Chatbot/chatbot.pl
@@ -68,7 +68,7 @@ display_diagnosis(Patient, Disease, Age, H, W, Temp, Sys, Dias, HR) :-
         (Disease \= null)->            
             format("The patient, ~w, is diagnosed with ~w based from the symptoms presented.", [Patient, Disease]), nl, nl, treatment(Disease), true;
             format("No diagnosis was found for ~w with the given symptoms.", [Patient]), nl, nl, true
-    ).
+    ), nl, nl.
 
 /*Structure for yes or no questions*/
 yesno(Patient, Question) :-
@@ -209,29 +209,92 @@ diagnosis(Patient, malaria, Sys, Dias) :- malaria(Patient) , !.
 diagnosis(Patient, pharyngitis, Sys, Dias) :- pharyngitis(Patient) , !.
 diagnosis(_, _, Sys, Dias).
 
+/*For writing list of treatments*/
+list_treatments([]).
+list_treatments([H|L]) :-
+    write(H),nl,list_treatments(L).
+
 /*Treatment assembly*/
 treatment(hypertension) :-
-    format("The treatment for ~w is ~w.~n", [hypertension, "vwx"]).
+    format("The treatment for ~w is ~w.~n~n", [hypertension]),
+    list_treatments([
+                        "1. Lifestyle-related changes", "1.1. Eating a heart-healthy diet with less salt.","1.2. Getting regular physical activity.", 
+                        "1.3. Maintaining a healthy weight or losing weight.", "1.4. Limiting alcohol intake.", "1.5. Avoid smoking.", 
+                        "1.6. Getting 7 to 9 hours of sleep daily.","2. Medications (as per doctor's prescription) such as: ",
+                        "2.1. Water pills (diuretics)", "2.2. Angiotensin-converting enzyme (ACE) inhibitors", "2.3. Angiotensin II receptor blockers (ARBs)", 
+                        "2.4. Calcium channel blockers", "2.5. Alpha blockers", "2.6. Alpha-beta blockers", "2.7. Beta blockers", "2.8. Aldosterone antagonists", 
+                        "2.9. Renin inhibitors", "2.10. Vasodilators", "2.11. Central-acting agents"
+                    ]).
 treatment(flu) :-
-    format("The treatment for ~w is ~w.~n", [flu, "test"]).
+    format("The treatment for ~w are:~n~n", [flu]),
+    list_treatments([
+                        "Urgent Treatment:", "Seek medical attention if fever is above 38.9C or if there are persistent coughs (may indicate pneumonia).", 
+                        "", "Common Treatment:", "1. Extended resting until symptoms are gone.", "2. Drink lots of clear fluids.", 
+                        "3. Don't smoke.", "4. Don't take antibiotics until specifically prescribed.", "", "Specific Treatment:", 
+                        "1. Runny/Stuffy Nose:", "1.1 Take decongestants.", 
+                        "1.2 Take salt-water nasal sprays.", "1.3 Use humidifiers or take hot showers.","2. Coughs", 
+                        "2.1 Take Dextromethorphan (consult with doctor first).", "2.2 Inhale steam/water vapor to loosen mucus.", "3. Sore Throat", 
+                        "3.1 Take Phenol found on lozenges and sprays.", "3.2 Gargling with warm salt water.", "3.3 Drink tea with lemon.", 
+                        "4. Fever/Pain - Take Acetaminophen, Aspirin, or Ibuprofen."
+                    ]).
 treatment(colds) :-
-    format("The treatment for ~w is ~w.~n", [colds, "wxy"]).
+    format("The treatment for ~w are:~n~n", [colds]),
+    list_treatments([
+						"1. Rest and sleep.", "2. Drink plenty of water (fruit juice or squash mixed with water is OK) to avoid dehydration.", 
+						"3. Gargle salt water to soothe a sore throat (not suitable for children)."
+                    ]).
 treatment(diarrhea) :-
-    format("The treatment for ~w is ~w.~n", [diarrhea, "uvw"]).
+    format("The treatment for ~w are:~n~n", [diarrhea]),
+    list_treatments([
+                        "1. Avoid milk-based, greasy, fibrous, or very sweet foods.", "2. Avoid caffeine and alcohol.", 
+                        "3. Do not eat solid foods if you have signs of dehydration.", "4. Drink fluids with electrolytes or sodium.", 
+                        "5. Eat normal foods within 12 hours that are non-irritating to the intesting (i.e., bland foods)."
+                    ]).
 treatment(tuberculosis) :-
-    format("The treatment for ~w is ~w.~n", [tuberculosis, "tuv"]).
+    format("The treatment for ~w are:~n~n", [tuberculosis]),
+    list_treatments([
+                        "1. Take medication (as per doctor's prescription)", "2. Stay at home with proper ventilation.", 
+                        "3. Cover your moth and wear a face mask."
+                    ]).
 treatment(pneumonia) :-
-    format("The treatment for ~w is ~w.~n", [pneumonia, "wxy"]).
+    format("The treatment for ~w are:~n~n", [pneumonia]),
+    list_treatments([
+                        "Note: Treatment will be a case-by-case basis, please consult your doctor for full information.", "",
+                        "1. Take antibiotics (as per doctor's prescription).", "2. Rest, drink a lot of water, and take medicine for fever.", 
+                        "3. Consult with a doctor or go to a hospital should the symptoms get worse especailly for difficulty of breathing."
+                    ]).
 treatment(diabetes) :-
-    format("The treatment for ~w is ~w.~n", [diabetes, "uvw"]).
+    format("The treatment for ~w are:~n~n", [diabetes]),
+    list_treatments([
+                        "1. Have a healthy diet.", "2. Do more physical activities.", "3. Avoid foods with sugar and saturated fats.", 
+                        "4. Avoid smoking.", "5. For Type 1 Diabetes: ", 
+                        "Consult doctor for recommendations for insulin injections as well as for orally administered drugs that aides the pancreas in producing more insulin."
+                    ]).
 treatment(measles) :-
-    format("The treatment for ~w is ~w.~n", [measles, "tuv"]).
+    format("The treatment for ~w are:~n~n", [measles]),
+    list_treatments([
+                        "1. Isolate yourself or people with measles.", "2. Vaccinate other people at home against measles, especially those that haven't contracted it yet."
+                    ]).
 treatment(dengue) :-
-    format("The treatment for ~w is ~w.~n", [dengue, "wxy"]).
+    format("The treatment for ~w are:~n~n", [dengue]),
+    list_treatments([   
+                        "Urgent care: Consult with a doctor or go to a hospital should symptoms worsen or stay for too long.", "", 
+                        "General Treatment: ", "1. Rest", "2. Stay Hydrated.", "3. If possible, take a dengue fever vaccine (ask your doctor for more information.", 
+                        "4. Avoid having a mosquito bites by covering your skin."
+                    ]).
 treatment(malaria) :-
-    format("The treatment for ~w is ~w.~n", [malaria, "uvw"]).
+    format("The treatment for ~w are:~n~n", [malaria]),
+    list_treatments([
+                        "1. Avoid having a mosquito bites by covering your skin.", "2. Take anti-malaria medical drugs or a vaccine (as per doctor's prescrition)."
+                    ]).
 treatment(pharyngitis) :-
-    format("The treatment for ~w is ~w.~n", [pharyngitis, "tuv"]).
+    format("The treatment for ~w are:~n~n", [pharyngitis]),
+    list_treatments([
+                        "Treatment will depend base on cause of the sore throat but here are the general treatments: ", "", "For viral pharyngitis: ", 
+                        "1. Take salt water gargles", "2. Take pain relievers", "3. Drink extra fluids (e.g., water)", "", "For bacterial/fungal pharyngitis: ", 
+                        "1. Treated with antibiotics or antifungal medications respectively (as per doctor's prescription)", 
+                        "2. Patients with strep throat are advised to consult with a doctor for possible antibiotic therapy."
+                    ]).
 
 /*Clear screen; Just call 'clear.'*/
 clear :- write("\33\[2J").
