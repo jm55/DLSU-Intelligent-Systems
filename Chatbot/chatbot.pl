677bb308ee0e0c91a58fcdae6249a4dde0c2923a
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-03-29 11:36:13 +0800

    Updated predicate execution and arrangement
    Modified in such a way that certain disease (which are also symptoms) are to be non-executed immediately in the process.
    
    This however has a side effect of sometimes experiencing a slight deviation to the conversation but will immediately return back on track should certain conditions be met.

--- Chatbot/chatbot.pl
+++ Chatbot/chatbot.pl
@@ -20,6 +20,7 @@ collect(Patient, Age, H, W, Temp, Sys, Dias, HR) :-
     header,
     write("What is the patient's name? "),
     read(Patient),
+    /*
     write("What is the patient's age ? "),
     read(Age),
     write("What is the patient's height (in cm)? "),
@@ -28,12 +29,16 @@ collect(Patient, Age, H, W, Temp, Sys, Dias, HR) :-
     read(W),
     write("What is the patient's temperature (in C)? "),
     read(Temp),
+    */
     write("What is the patient's systolic blood pressure (in mmHg)? "),
     read(Sys),
     write("What is the patient's diastolic blood pressure (in mmHg)? "),
-    read(Dias),
+    read(Dias)
+    /*,
     write("What is the patient's heartrate (in bpm)? "),
-    read(HR).
+    read(HR)
+    */
+    .
 
 
 /*Consider this as the main function*/
@@ -89,7 +94,6 @@ check(Patient, Question) :-
     ).
 
 /*Symptom facts gathering*/
-
 colds(Patient) :-
     check(Patient, "have Dry / Wet Cough (y/n)?"),
     check(Patient, "have Runny Nose (y/n)?"),
@@ -97,12 +101,11 @@ colds(Patient) :-
     check(Patient, "have Sore Throat (y/n)?").
 
 flu(Patient) :-
+    colds(Patient),
     check(Patient, "have Fever (y/n)?"),
     check(Patient, "have Fatigue / Weakness / Tiredness (y/n)?"),
     check(Patient, "have Body Pain (y/n)?"),
-    check(Patient, "have Headaches (y/n)?"),
-    /*Try to implement: OR colds*/
-    check(Patient, "have Colds (y/n)?").
+    check(Patient, "have Headaches (y/n)?").
 
 diarrhea(Patient) :-
     check(Patient, "have Fever (y/n)?"),
@@ -117,17 +120,16 @@ tuberculosis(Patient) :-
     check(Patient, "have Chest pains (y/n)?"),
     check(Patient, "have Sweating / Shivering / Chills (y/n)?"),
     check(Patient, "have Loss off Appetite (y/n)?"),
-    check(Patient, "have Chough with blood (y/n)?"),
+    check(Patient, "have Cough with blood (y/n)?"),
     check(Patient, "have Unexplained weight loss (y/n)?").
 
 pneumonia(Patient) :-
+    diarrhea(Patient),
     check(Patient, "have Fever (y/n)?"),
     check(Patient, "have Fatigue / Weakness / Tiredness (y/n)?"),
     check(Patient, "have Chest pains (y/n)?"),
     check(Patient, "have Sweating / Shivering / Chills (y/n)?"),
-    check(Patient, "have Wheezing / Breathlessness (y/n)?"),
-    /*OR Diarrhea*/
-    check(Patient, "have Diarrhea (y/n)?").
+    check(Patient, "have Wheezing / Breathlessness (y/n)?").
 
 diabetes(Patient) :-
     check(Patient, "have Fatigue / Weakness / Tiredness (y/n)?"),
@@ -140,29 +142,23 @@ diabetes(Patient) :-
     check(Patient, "have Slow-healing sores (y/n)?").
 
 measles(Patient) :-
+    flu(Patient),
     check(Patient, "have Fever (y/n)?"),
     check(Patient, "have Runny Nose (y/n)?"),
     check(Patient, "have Sore Throat (y/n)?"),
     check(Patient, "have Skin Rashes (y/n)?"),
     check(Patient, "have Conjunctivities (y/n)?"),
-    /*Not sure how to display this*/
-    check(Patient, "have Tiny white spots (y/n)?"),
-    /*OR flu*/
-    check(Patient, "have Flu (y/n)?").
+    check(Patient, "have Tiny white spots (y/n)?").
 
 dengue(Patient) :-
-    check(Patient, "have Fever (y/n)?"),
-    check(Patient, "have Fatigue / Weakness / Tiredness (y/n)?"),
-    check(Patient, "have Body Pain (y/n)?"),
-    check(Patient, "have Runny Nose (y/n)?"),
+    flu(patient),
+    /*Succeeding symptoms for dengue*/
     check(Patient, "have Vomiting (y/n)?"),
     check(Patient, "have Blood on vomit (y/n)?"),
     check(Patient, "have Swollen glands (y/n)?"),
     check(Patient, "have Bleeding gums or nose (y/n)?"),
     check(Patient, "have Persisting vomiting (y/n)?"),
-    check(Patient, "have Abdominal Pain (y/n)?"),
-    /*OR flu*/
-    check(Patient, "have Flu (y/n)?").
+    check(Patient, "have Abdominal Pain (y/n)?").
 
 malaria(Patient)  :-
     check(Patient, "have Fever (y/n)?"),
@@ -175,9 +171,9 @@ malaria(Patient)  :-
     check(Patient, "have Persisting vomiting (y/n)?"),
     check(Patient, "have Abdominal Pain (y/n)?").
 
-hypertension(Sys, Dias) :-
-    Sys >= 140,
-    Dias >= 90.
+hypertension(Patient, Sys, Dias) :-
+    (Sys >= 140,Dias >= 90); 
+    (check(Patient, "have an average BP of >=130/85 at home (y/n)?"); check(Patient, "had a one time BP measurement of 180/110 (y/n)? ")).
 
 pharyngitis(Patient)  :-
     check(Patient, "have Sore Throat (y/n)?"),
@@ -187,17 +183,17 @@ pharyngitis(Patient)  :-
 
 
 /*Diagnosis assembly*/
-diagnosis(Patient, hypertension, Sys, Dias) :- hypertension(Sys, Dias), !.
-diagnosis(Patient, colds, Sys, Dias) :- colds(Patient), !.
-diagnosis(Patient, flu, Sys, Dias) :- flu(Patient) , !.
-diagnosis(Patient, diarrhea, Sys, Dias) :- diarrhea(Patient) , !.
+diagnosis(Patient, hypertension, Sys, Dias) :- hypertension(Patient, Sys, Dias), !.
+/*diagnosis(Patient, colds, Sys, Dias) :- colds(Patient), !.*/
+diagnosis(Patient, dengue, Sys, Dias) :- dengue(Patient) , !.
 diagnosis(Patient, tuberculosis, Sys, Dias) :- tuberculosis(Patient) , !.
 diagnosis(Patient, pneumonia, Sys, Dias) :- pneumonia(Patient) , !.
 diagnosis(Patient, diabetes, Sys, Dias) :- diabetes(Patient) , !.
 diagnosis(Patient, measles, Sys, Dias) :- measles(Patient) , !.
-diagnosis(Patient, dengue, Sys, Dias) :- dengue(Patient) , !.
 diagnosis(Patient, malaria, Sys, Dias) :- malaria(Patient) , !.
 diagnosis(Patient, pharyngitis, Sys, Dias) :- pharyngitis(Patient) , !.
+diagnosis(Patient, flu, Sys, Dias) :- flu(Patient) , !.
+diagnosis(Patient, diarrhea, Sys, Dias) :- diarrhea(Patient) , !.
 diagnosis(_, _, Sys, Dias).
 
 /*For writing list of treatments*/
