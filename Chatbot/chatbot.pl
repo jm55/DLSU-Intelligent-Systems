191cf807c21f35a9cb1edab3401cabd9739d2f5d
Carla Jeannine Cruzada <80302664+CJOC3@users.noreply.github.com>
2023-03-31 10:47:08 +0800

    arranged no to fever branches according to visualization

--- Chatbot/chatbot.pl
+++ Chatbot/chatbot.pl
@@ -42,7 +42,7 @@ checkup :-
     diagnosis(Patient, Disease, Sys, Dias), nl,
     display_diagnosis(Patient, Disease, Age, H, W, Temp, Sys, Dias, HR),
     undo.
-    
+
 /*Display the collected health data of patient*/
 display_data(Patient, Age, H, W, Temp, Sys, Dias, HR) :-
     format("Patient: ~w", [Patient]), nl,
@@ -59,7 +59,7 @@ display_diagnosis(Patient, Disease, Age, H, W, Temp, Sys, Dias, HR) :-
     /*display_data(Patient, Age, H, W, Temp, Sys, Dias, HR),*/
     (
         /*Display the Disease if is not null, else display no diagnosis*/
-        (Disease \= null)->            
+        (Disease \= null)->
             format("The patient, ~w, is diagnosed with ~w based from the symptoms presented.", [Patient, Disease]), nl, nl, treatment(Disease), true;
             format("No diagnosis was found for ~w with the given symptoms.", [Patient]), nl, nl, true
     ), nl, nl.
@@ -89,20 +89,30 @@ check(Patient, Question) :-
     ).
 
 /*Symptom facts gathering*/
-
-colds(Patient) :-
-    check(Patient, "have Dry / Wet Cough (y/n)?"),
-    check(Patient, "have Runny Nose (y/n)?"),
-    check(Patient, "have Sneezing (y/n)?"),
-    check(Patient, "have Sore Throat (y/n)?").
-
 flu(Patient) :-
     check(Patient, "have Fever (y/n)?"),
     check(Patient, "have Fatigue / Weakness / Tiredness (y/n)?"),
     check(Patient, "have Body Pain (y/n)?"),
-    check(Patient, "have Headaches (y/n)?"),
+    check(Patient, "have Headaches (y/n)?").
+
     /*Try to implement: OR colds*/
-    check(Patient, "have Colds (y/n)?").
+    /*check(Patient, "have Colds (y/n)?"). */
+
+pharyngitis(Patient)  :-
+    check(Patient, "have Sore Throat (y/n)?"),
+    check(Patient, "have Dry / Scratchy throat (y/n)?"),
+    check(Patient, "have Pain when swallowing (y/n)?"),
+    check(Patient, "have Pain when speaking (y/n)?").
+
+diabetes(Patient) :-
+    check(Patient, "have Polydipsia (Excess thrist) (y/n)?"),
+    check(Patient, "have Polyphagia (Always hungry) (y/n)?"),
+    check(Patient, "have Polyuria (Frequent urination) (y/n)?"),
+    check(Patient, "have Unexplained weight loss (y/n)?"),
+    check(Patient, "have Blurry vision (y/n)?"),
+    check(Patient, "have Irritable / moody (y/n)?"),
+    check(Patient, "have Fatigue / Weakness / Tiredness (y/n)?"),
+    check(Patient, "have Slow-healing sores (y/n)?").
 
 diarrhea(Patient) :-
     check(Patient, "have Fever (y/n)?"),
@@ -129,15 +139,7 @@ pneumonia(Patient) :-
     /*OR Diarrhea*/
     check(Patient, "have Diarrhea (y/n)?").
 
-diabetes(Patient) :-
-    check(Patient, "have Fatigue / Weakness / Tiredness (y/n)?"),
-    check(Patient, "have Unexplained weight loss (y/n)?"),
-    check(Patient, "have Blurry vision (y/n)?"),
-    check(Patient, "have Irritable / moody (y/n)?"),
-    check(Patient, "have Polydipsia (Excess thrist) (y/n)?"),
-    check(Patient, "have Polyphagia (Always hungry) (y/n)?"),
-    check(Patient, "have Polyuria (Frequent urination) (y/n)?"),
-    check(Patient, "have Slow-healing sores (y/n)?").
+
 
 measles(Patient) :-
     check(Patient, "have Fever (y/n)?"),
@@ -160,9 +162,9 @@ dengue(Patient) :-
     check(Patient, "have Swollen glands (y/n)?"),
     check(Patient, "have Bleeding gums or nose (y/n)?"),
     check(Patient, "have Persisting vomiting (y/n)?"),
-    check(Patient, "have Abdominal Pain (y/n)?"),
+    check(Patient, "have Abdominal Pain (y/n)?").
     /*OR flu*/
-    check(Patient, "have Flu (y/n)?").
+    /*check(Patient, "have Flu (y/n)?"). */
 
 malaria(Patient)  :-
     check(Patient, "have Fever (y/n)?"),
@@ -175,29 +177,24 @@ malaria(Patient)  :-
     check(Patient, "have Persisting vomiting (y/n)?"),
     check(Patient, "have Abdominal Pain (y/n)?").
 
-hypertension(Sys, Dias) :-
-    Sys >= 140,
-    Dias >= 90.
+hypertension(Patient, Sys, Dias) :-
+    (Sys >= 140, Dias >= 90);
+    (check(Patient, "have an average BP of >=130/85 at home (y/n)?");
+     check(Patient, "had a one time BP measurement of 180/110 (y/n)? ")).
 
-pharyngitis(Patient)  :-
-    check(Patient, "have Sore Throat (y/n)?"),
-    check(Patient, "have Dry / Scratchy throat (y/n)?"),
-    check(Patient, "have Pain when swallowing (y/n)?"),
-    check(Patient, "have Pain when speaking (y/n)?").
 
 
 /*Diagnosis assembly*/
-diagnosis(Patient, hypertension, Sys, Dias) :- hypertension(Sys, Dias), !.
-diagnosis(Patient, colds, Sys, Dias) :- colds(Patient), !.
-diagnosis(Patient, flu, Sys, Dias) :- flu(Patient) , !.
+diagnosis(Patient, hypertension, Sys, Dias) :- hypertension(Patient, Sys, Dias), !.
+diagnosis(Patient, flu, Sys, Dias) :- flu(Patient), !.
+diagnosis(Patient, pharyngitis, Sys, Dias) :- pharyngitis(Patient) , !.
+diagnosis(Patient, diabetes, Sys, Dias) :- diabetes(Patient) , !.
 diagnosis(Patient, diarrhea, Sys, Dias) :- diarrhea(Patient) , !.
 diagnosis(Patient, tuberculosis, Sys, Dias) :- tuberculosis(Patient) , !.
 diagnosis(Patient, pneumonia, Sys, Dias) :- pneumonia(Patient) , !.
-diagnosis(Patient, diabetes, Sys, Dias) :- diabetes(Patient) , !.
 diagnosis(Patient, measles, Sys, Dias) :- measles(Patient) , !.
 diagnosis(Patient, dengue, Sys, Dias) :- dengue(Patient) , !.
 diagnosis(Patient, malaria, Sys, Dias) :- malaria(Patient) , !.
-diagnosis(Patient, pharyngitis, Sys, Dias) :- pharyngitis(Patient) , !.
 diagnosis(_, _, Sys, Dias).
 
 /*For writing list of treatments*/
@@ -209,55 +206,55 @@ list_treatments([H|L]) :-
 treatment(hypertension) :-
     format("The treatment for ~w are:~n~n", [hypertension]),
     list_treatments([
-                        "1. Lifestyle-related changes", "1.1. Eating a heart-healthy diet with less salt.","1.2. Getting regular physical activity.", 
-                        "1.3. Maintaining a healthy weight or losing weight.", "1.4. Limiting alcohol intake.", "1.5. Avoid smoking.", 
+                        "1. Lifestyle-related changes", "1.1. Eating a heart-healthy diet with less salt.","1.2. Getting regular physical activity.",
+                        "1.3. Maintaining a healthy weight or losing weight.", "1.4. Limiting alcohol intake.", "1.5. Avoid smoking.",
                         "1.6. Getting 7 to 9 hours of sleep daily.","2. Medications (as per doctor's prescription) such as: ",
-                        "2.1. Water pills (diuretics)", "2.2. Angiotensin-converting enzyme (ACE) inhibitors", "2.3. Angiotensin II receptor blockers (ARBs)", 
-                        "2.4. Calcium channel blockers", "2.5. Alpha blockers", "2.6. Alpha-beta blockers", "2.7. Beta blockers", "2.8. Aldosterone antagonists", 
+                        "2.1. Water pills (diuretics)", "2.2. Angiotensin-converting enzyme (ACE) inhibitors", "2.3. Angiotensin II receptor blockers (ARBs)",
+                        "2.4. Calcium channel blockers", "2.5. Alpha blockers", "2.6. Alpha-beta blockers", "2.7. Beta blockers", "2.8. Aldosterone antagonists",
                         "2.9. Renin inhibitors", "2.10. Vasodilators", "2.11. Central-acting agents"
                     ]).
 treatment(flu) :-
     format("The treatment for ~w are:~n~n", [flu]),
     list_treatments([
-                        "Urgent Treatment:", "Seek medical attention if fever is above 38.9C or if there are persistent coughs (may indicate pneumonia).", 
-                        "", "Common Treatment:", "1. Extended resting until symptoms are gone.", "2. Drink lots of clear fluids.", 
-                        "3. Don't smoke.", "4. Don't take antibiotics until specifically prescribed.", "", "Specific Treatment:", 
-                        "1. Runny/Stuffy Nose:", "1.1 Take decongestants.", 
-                        "1.2 Take salt-water nasal sprays.", "1.3 Use humidifiers or take hot showers.","2. Coughs", 
-                        "2.1 Take Dextromethorphan (consult with doctor first).", "2.2 Inhale steam/water vapor to loosen mucus.", "3. Sore Throat", 
-                        "3.1 Take Phenol found on lozenges and sprays.", "3.2 Gargling with warm salt water.", "3.3 Drink tea with lemon.", 
+                        "Urgent Treatment:", "Seek medical attention if fever is above 38.9C or if there are persistent coughs (may indicate pneumonia).",
+                        "", "Common Treatment:", "1. Extended resting until symptoms are gone.", "2. Drink lots of clear fluids.",
+                        "3. Don't smoke.", "4. Don't take antibiotics until specifically prescribed.", "", "Specific Treatment:",
+                        "1. Runny/Stuffy Nose:", "1.1 Take decongestants.",
+                        "1.2 Take salt-water nasal sprays.", "1.3 Use humidifiers or take hot showers.","2. Coughs",
+                        "2.1 Take Dextromethorphan (consult with doctor first).", "2.2 Inhale steam/water vapor to loosen mucus.", "3. Sore Throat",
+                        "3.1 Take Phenol found on lozenges and sprays.", "3.2 Gargling with warm salt water.", "3.3 Drink tea with lemon.",
                         "4. Fever/Pain - Take Acetaminophen, Aspirin, or Ibuprofen."
                     ]).
 treatment(colds) :-
     format("The treatment for ~w are:~n~n", [colds]),
     list_treatments([
-						"1. Rest and sleep.", "2. Drink plenty of water (fruit juice or squash mixed with water is OK) to avoid dehydration.", 
+						"1. Rest and sleep.", "2. Drink plenty of water (fruit juice or squash mixed with water is OK) to avoid dehydration.",
 						"3. Gargle salt water to soothe a sore throat (not suitable for children)."
                     ]).
 treatment(diarrhea) :-
     format("The treatment for ~w are:~n~n", [diarrhea]),
     list_treatments([
-                        "1. Avoid milk-based, greasy, fibrous, or very sweet foods.", "2. Avoid caffeine and alcohol.", 
-                        "3. Do not eat solid foods if you have signs of dehydration.", "4. Drink fluids with electrolytes or sodium.", 
+                        "1. Avoid milk-based, greasy, fibrous, or very sweet foods.", "2. Avoid caffeine and alcohol.",
+                        "3. Do not eat solid foods if you have signs of dehydration.", "4. Drink fluids with electrolytes or sodium.",
                         "5. Eat normal foods within 12 hours that are non-irritating to the intesting (i.e., bland foods)."
                     ]).
 treatment(tuberculosis) :-
     format("The treatment for ~w are:~n~n", [tuberculosis]),
     list_treatments([
-                        "1. Take medication (as per doctor's prescription)", "2. Stay at home with proper ventilation.", 
+                        "1. Take medication (as per doctor's prescription)", "2. Stay at home with proper ventilation.",
                         "3. Cover your moth and wear a face mask."
                     ]).
 treatment(pneumonia) :-
     format("The treatment for ~w are:~n~n", [pneumonia]),
     list_treatments([
                         "Note: The treatment will be a case-by-case basis, please consult your doctor for full information.", "",
-                        "1. Take antibiotics (as per doctor's prescription).", "2. Rest, drink a lot of water, and take medicine for fever.", 
+                        "1. Take antibiotics (as per doctor's prescription).", "2. Rest, drink a lot of water, and take medicine for fever.",
                         "3. Consult with a doctor or go to a hospital should the symptoms get worse especailly for difficulty of breathing."
                     ]).
 treatment(diabetes) :-
     format("The treatment for ~w are:~n~n", [diabetes]),
     list_treatments([
-                        "1. Have a healthy diet.", "2. Do more physical activities.", "3. Avoid foods with sugar and saturated fats.", 
+                        "1. Have a healthy diet.", "2. Do more physical activities.", "3. Avoid foods with sugar and saturated fats.",
                         "4. Avoid smoking.", "5. For Type 1 Diabetes: Consult doctor for recommendations for insulin injections as well as for orally administered drugs that aides the pancreas in producing more insulin."
                     ]).
 treatment(measles) :-
@@ -267,9 +264,9 @@ treatment(measles) :-
                     ]).
 treatment(dengue) :-
     format("The treatment for ~w are:~n~n", [dengue]),
-    list_treatments([   
-                        "Urgent treatment: Consult with a doctor or go to a hospital should symptoms worsen or stay for too long.", "", 
-                        "Common Treatment: ", "1. Rest", "2. Stay Hydrated.", "3. If possible, take a dengue fever vaccine (ask your doctor for more information.", 
+    list_treatments([
+                        "Urgent treatment: Consult with a doctor or go to a hospital should symptoms worsen or stay for too long.", "",
+                        "Common Treatment: ", "1. Rest", "2. Stay Hydrated.", "3. If possible, take a dengue fever vaccine (ask your doctor for more information.",
                         "4. Avoid having a mosquito bites by covering your skin."
                     ]).
 treatment(malaria) :-
@@ -281,9 +278,9 @@ treatment(pharyngitis) :-
     format("The treatment for ~w are:~n~n", [pharyngitis]),
     list_treatments([
                         "Note: The treatment will depend base on cause of the sore throat, please consult your doctor for further information.",
-                        "Nonetheless, the following are the general treatments based on cause.", "", "For viral pharyngitis: ", 
-                        "1. Take salt water gargles", "2. Take pain relievers", "3. Drink extra fluids (e.g., water)", "", "For bacterial/fungal pharyngitis: ", 
-                        "1. Treated with antibiotics or antifungal medications respectively (as per doctor's prescription)", 
+                        "Nonetheless, the following are the general treatments based on cause.", "", "For viral pharyngitis: ",
+                        "1. Take salt water gargles", "2. Take pain relievers", "3. Drink extra fluids (e.g., water)", "", "For bacterial/fungal pharyngitis: ",
+                        "1. Treated with antibiotics or antifungal medications respectively (as per doctor's prescription)",
                         "2. Patients with strep throat are advised to consult with a doctor for possible antibiotic therapy."
                     ]).
 
